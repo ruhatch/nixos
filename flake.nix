cca1b4abf6c0d577cf569cc12a1a95e6fe6ce15c
Ru Horlick <ruperthorlick@gmail.com>
2021-03-21 12:13:54 +0000

    Install onlyoffice

--- flake.nix
+++ flake.nix
@@ -1,14 +1,17 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
   inputs.nixos-hardware.url = github:NixOS/nixos-hardware;
+  inputs.onlyoffice.url = github:GTrunSec/onlyoffice-desktopeditors-flake;
 
-  outputs = { self, nixpkgs, nixos-hardware }: {
-    nixosConfigurations.delilah = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, nixos-hardware, onlyoffice }: {
+    nixosConfigurations.delilah = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       modules = [
         nixos-hardware.nixosModules.dell-xps-15-9550
         ./configuration.nix
         ({
+          environment.systemPackages = [ onlyoffice.defaultPackage."${system}" ];
+
           system.configurationRevision =
             if self ? rev
             then self.rev
