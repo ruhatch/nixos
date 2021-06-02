bb3c4d195953e195ffae13fe53d2219d65b5e7bb
Ru Horlick <ruperthorlick@gmail.com>
2021-06-02 14:19:17 +0100

    Package starport and add to system packages

--- flake.nix
+++ flake.nix
@@ -1,16 +1,20 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
+  inputs.nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   inputs.nixos-hardware.url = github:NixOS/nixos-hardware;
   inputs.onlyoffice.url = github:GTrunSec/onlyoffice-desktopeditors-flake;
 
-  outputs = { self, nixpkgs, nixos-hardware, onlyoffice }: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware, onlyoffice }: {
     nixosConfigurations.delilah = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       modules = [
         nixos-hardware.nixosModules.dell-xps-15-9550
         ./configuration.nix
         ({
-          environment.systemPackages = [ onlyoffice.defaultPackage."${system}" ];
+          environment.systemPackages = [
+            onlyoffice.defaultPackage."${system}"
+            ((import nixpkgs { inherit system; }).callPackage ./starport.nix { inherit (import nixpkgs-unstable { inherit system; }) buildGoModule; })
+          ];
 
           system.configurationRevision =
             if self ? rev
