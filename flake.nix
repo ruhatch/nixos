495b918a5dec40b5d515c6bd5e6fe5518b9ad474
Ru Horlick <ruperthorlick@gmail.com>
2023-05-21 12:05:28 +0100

    Add minecraft flake

--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
   inputs.nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   inputs.unfree-nixpkgs.url = path:unfree-nixpkgs;
   inputs.nixos-hardware.url = github:NixOS/nixos-hardware;
+  inputs.minecraft-nix.url = github:Ninlives/minecraft.nix;
 
-  outputs = { self, nixpkgs, unfree-nixpkgs, nixpkgs-unstable, nixos-hardware }: {
+  outputs = { self, nixpkgs, unfree-nixpkgs, nixpkgs-unstable, nixos-hardware, minecraft-nix }: {
     nixosConfigurations.delilah = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       modules = [
@@ -23,6 +24,7 @@
 
           nix.registry.nixpkgs.flake = nixpkgs;
           nix.registry.unfree-nixpkgs.flake = unfree-nixpkgs;
+          nix.registry.minecraft.flake = minecraft-nix;
         })
       ];
     };
