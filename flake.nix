edadef2c1a3a45259271b5e656843b39c79796f9
Ru Horlick <ruperthorlick@gmail.com>
2023-05-29 00:18:05 +0100

    Add 2020 nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
+  inputs.nixpkgs-20.url = github:NixOS/nixpkgs/nixos-20.03;
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
   inputs.nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   inputs.unfree-nixpkgs.url = path:unfree-nixpkgs;
   inputs.nixos-hardware.url = github:NixOS/nixos-hardware;
   inputs.minecraft-nix.url = github:Ninlives/minecraft.nix;
 
-  outputs = { self, nixpkgs, unfree-nixpkgs, nixpkgs-unstable, nixos-hardware, minecraft-nix }: {
+  outputs = { self, nixpkgs-20, nixpkgs, unfree-nixpkgs, nixpkgs-unstable, nixos-hardware, minecraft-nix }: {
     nixosConfigurations.delilah = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       modules = [
@@ -23,6 +24,7 @@
 	  ];
 
           nix.registry.nixpkgs.flake = nixpkgs;
+          nix.registry.nixpkgs-20.flake = nixpkgs-20;
           nix.registry.unfree-nixpkgs.flake = unfree-nixpkgs;
           nix.registry.minecraft.flake = minecraft-nix;
         })
