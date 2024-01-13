b58bcf6a25e75c879bd3a4c4756749d8c0d8b773
Ru Horlick <ruperthorlick@gmail.com>
2024-01-13 11:18:46 +0000

    Update to 23.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs-20.url = github:NixOS/nixpkgs/nixos-20.09;
-  inputs.nixpkgs.url = github:NixOS/nixpkgs?nixos-23.05;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs?nixos-23.11;
   inputs.nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   inputs.unfree-nixpkgs.url = path:unfree-nixpkgs;
   inputs.nixos-hardware.url = github:NixOS/nixos-hardware;
