4663c09ac6e84e5ee1d547e98a5ecf634f20e733
Ru Horlick <ruperthorlick@gmail.com>
2024-03-25 18:21:36 +0000

    Add tempfiles.d for glibc

--- configuration.nix
+++ configuration.nix
@@ -430,6 +430,9 @@ in
   };
 
   systemd = {
+    tmpfiles.rules = [
+      "L+ /lib64/ld-linux-x86-64.so.2 - - - - ${pkgs.glibc}/lib64/ld-linux-x86-64.so.2"
+    ];
     user.services = {
       "libinput-gestures" = {
         description = "Add multitouch gestures using libinput-gestures";
