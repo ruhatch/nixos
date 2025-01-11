e2f58df2c23d80a941ec718ef627341daede012b
Ru Horlick <ruperthorlick@gmail.com>
2025-01-11 09:38:13 +0000

    Remove unfree-nixpkgs

--- configuration.nix
+++ configuration.nix
@@ -257,6 +257,8 @@ in
     openssh.enable = true;
     pipewire = {
       enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
       pulse.enable = true;
     };
 
--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
   inputs.nixpkgs-20.url = github:NixOS/nixpkgs/nixos-20.09;
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
   inputs.nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-  inputs.unfree-nixpkgs.url = path:unfree-nixpkgs;
   inputs.nixos-hardware.url = github:NixOS/nixos-hardware;
 
-  outputs = { self, nixpkgs-20, nixpkgs, unfree-nixpkgs, nixpkgs-unstable, nixos-hardware }: {
+  outputs = { self, nixpkgs-20, nixpkgs, nixpkgs-unstable, nixos-hardware }: {
     nixosConfigurations.delilah = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       modules = [
@@ -24,7 +23,6 @@
 
           nix.registry.nixpkgs.flake = nixpkgs;
           nix.registry.nixpkgs-20.flake = nixpkgs-20;
-          nix.registry.unfree-nixpkgs.flake = unfree-nixpkgs;
         })
       ];
     };
